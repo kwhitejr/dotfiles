f856f2eff6839dd77325994b86abf3579df5a726
Kevin White <kevin.white@nike.com>
2018-01-13 20:19:15 -0800

    removed unnecessary apps from cask install

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -13,24 +13,15 @@ brew cask install slack
 brew cask install discord
 brew cask install karabiner
 brew cask install google-chrome
-brew cask install firefox
 brew cask install 1password
 brew cask install flux
 brew cask install bartender
 brew cask install gyazo
-brew cask install rescuetime
 brew cask install sublime-text
-brew cask install atom
-brew cask install imageoptim
-brew cask install vlc
-brew cask install licecap
 brew cask install caffeine
-brew cask install screenhero
-brew cask install skitch
 brew cask install sketch
 brew cask install sketch-toolbox
-brew cask install vmware-fusion
-brew cask install lumen
+# brew cask install vmware-fusion # runs windows on mac
 
 # https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
